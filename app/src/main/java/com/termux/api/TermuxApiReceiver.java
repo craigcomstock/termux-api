cb3a16f00cfd2dd367e4440472f89cdc8453fd31
Craig Comstock <craig@unreasonablefarm.org>
2025-04-01 20:07:15 -0500

    MMS message sending wasn't working maybe due to lack of MANAGE_SUBSCRIPTION_USER_ASSOCIATION

--- app/src/main/java/com/termux/api/TermuxApiReceiver.java
+++ app/src/main/java/com/termux/api/TermuxApiReceiver.java
@@ -212,7 +212,7 @@ public class TermuxApiReceiver extends BroadcastReceiver {
                 }
                 break;
             case "MmsSetup":
-                if (TermuxApiPermissionActivity.checkAndRequestPermissions(context, intent, Manifest.permission.RECEIVE_MMS, Manifest.permission.RECEIVE_WAP_PUSH)) {
+                if (TermuxApiPermissionActivity.checkAndRequestPermissions(context, intent, Manifest.permission.RECEIVE_MMS, Manifest.permission.RECEIVE_WAP_PUSH, Manifest.permission.MANAGE_SUBSCRIPTION_USER_ASSOCIATION)) {
                     MmsSetupAPI.onReceive(this, context, intent);
                 }
                 break;
